b1b7a87e54866782ad9e9dd0c7911a5d361f439f
Alejandro Henestroza <alehenestroza@gmail.com>
2023-11-09 19:42:11 -0300

    test: read csv data

---
+++ internal/reader/reader_test.go
@@ -0,0 +1,46 @@
+package reader_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/alehenestroza/stori-backend-challenge/internal/reader"
+)
+
+func TestCsvLoaderLoad(t *testing.T) {
+	tmpFile, err := os.CreateTemp("", "test.csv")
+	if err != nil {
+		t.Fatalf("error creating temp file: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	data := "header1,header2\nvalue1,value2\nvalue3,value4"
+	if _, err := tmpFile.Write([]byte(data)); err != nil {
+		t.Fatalf("error writing to temp file: %v", err)
+	}
+
+	loader := reader.NewCsvDataReader()
+
+	if err := loader.Read(tmpFile.Name()); err != nil {
+		t.Errorf("unable to load file: %v", err)
+	}
+
+	expected := []string{"value1,value2", "value3,value4"}
+	if !compareSlices(loader.Records, expected) {
+		t.Errorf("got %v but expected %v", loader.Records, expected)
+	}
+}
+
+func compareSlices(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
