13c07ea61d1d04567e203d9043f5b78917012967
Alejandro Henestroza <alehenestroza@gmail.com>
2023-11-09 20:01:41 -0300

    [FTR] Csv-Transaction parser (#3)
    * feat: transaction csv parser
    
    * test: parse csv to transactions

---
+++ internal/parser/transaction_parser.go
@@ -0,0 +1,55 @@
+package parser
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/alehenestroza/stori-backend-challenge/internal/transaction"
+)
+
+type TransactionParser struct{}
+
+func NewTransactionParser() TransactionParser {
+	return TransactionParser{}
+}
+
+func (tp TransactionParser) Parse(rows []string) ([]transaction.Transaction, error) {
+	transactions := make([]transaction.Transaction, len(rows))
+
+	for i, row := range rows {
+		id, date, amount, err := tp.parseRow(row)
+		if err != nil {
+			fmt.Printf("could not parse line %d: %s. Error: %v", i, row, err)
+		}
+
+		transactions[i] = transaction.Transaction{
+			Id:     int32(id),
+			Date:   date,
+			Amount: float32(amount),
+		}
+	}
+
+	return transactions, nil
+}
+
+func (tp TransactionParser) parseRow(row string) (int32, string, float32, error) {
+	parts := strings.Split(row, ",")
+	if len(parts) != 3 {
+		return 0, "", 0, fmt.Errorf("wrong input format")
+	}
+
+	id, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return 0, "", 0, fmt.Errorf("could not parse transaction ID")
+	}
+
+	date := parts[1]
+
+	amount, err := strconv.ParseFloat(parts[2], 32)
+	if err != nil {
+		return 0, "", 0, fmt.Errorf("could not parse transaction Amount")
+	}
+
+	return int32(id), date, float32(amount), nil
+}
