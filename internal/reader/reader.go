003fb7d10d1c601f3f49cc8750ea5e4372006b06
Alejandro Henestroza <alehenestroza@gmail.com>
2023-11-09 19:38:48 -0300

    feat: csv reader

---
+++ internal/reader/reader.go
@@ -0,0 +1,34 @@
+package reader
+
+import (
+	"errors"
+	"os"
+	"strings"
+)
+
+type CsvDataReader struct {
+	Records []string
+}
+
+func NewCsvLoader() *CsvDataReader {
+	return &CsvDataReader{}
+}
+
+func (csvl *CsvDataReader) Load(path string) error {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	rawString := string(file)
+	csvl.Records = strings.Split(rawString, "\n")[1:] // Skip first line, which contains the headers of the CSV file
+
+	return nil
+}
+
+func (csvl *CsvDataReader) GetRecords() ([]string, error) {
+	if len(csvl.Records) == 0 {
+		return nil, errors.New("no records present")
+	}
+	return csvl.Records, nil
+}
