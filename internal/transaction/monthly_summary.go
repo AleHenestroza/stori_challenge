a92581cacce598d886aa8a9a785513568260762a
Alejandro Henestroza <alehenestroza@gmail.com>
2023-11-09 19:21:19 -0300

    feat: monthly summary

---
+++ internal/transaction/monthly_summary.go
@@ -0,0 +1,46 @@
+package transaction
+
+import (
+	"time"
+)
+
+type MonthlySummary struct {
+	Month   string
+	Balance float32
+}
+
+func NewMonthlySummary(txns []Transaction, month string) (MonthlySummary, error) {
+	balance, err := calculateMonthlyBalance(txns, month)
+	if err != nil {
+		return MonthlySummary{}, err
+	}
+
+	summary := MonthlySummary{
+		Month:   month,
+		Balance: balance,
+	}
+
+	return summary, nil
+}
+
+func calculateMonthlyBalance(txns []Transaction, month string) (float32, error) {
+	var totalBalance float32
+
+	for _, t := range txns {
+		date, err := t.GetDate()
+		if err != nil {
+			return 0, err
+		}
+
+		m, err := time.Parse("January", month)
+		if err != nil {
+			return 0, err
+		}
+
+		if date.Month() == m.Month() {
+			totalBalance += t.Amount
+		}
+	}
+
+	return totalBalance, nil
+}
