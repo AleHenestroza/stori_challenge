2c78ccd705d23aff468ce57034ad26dc376624fc
Alejandro Henestroza <alehenestroza@gmail.com>
2023-11-09 23:25:20 -0300

    feat: add dependencies to application struct

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -8,13 +8,23 @@ import (
 	"os"
 	"time"
 
-	"github.com/alehenestroza/stori-backend-challenge/internal/reader"
+	"github.com/alehenestroza/stori-backend-challenge/internal/mailer"
 	"github.com/alehenestroza/stori-backend-challenge/internal/parser"
+	"github.com/alehenestroza/stori-backend-challenge/internal/reader"
 )
 
 type config struct {
 	port int
 	env  string
+	smtp smtp
+}
+
+type smtp struct {
+	host     string
+	port     int
+	username string
+	password string
+	sender   string
 }
 
 type application struct {
@@ -22,6 +32,8 @@ type application struct {
 	logger    *slog.Logger
 	csvLoader reader.CsvDataReader
 	parser    parser.TransactionParser
+	mailer    mailer.Mailer
+	formater  mailer.Formater
 }
 
 func main() {
@@ -29,15 +41,27 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|production)")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "<username>", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "<password>", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Stori Test <no-reply@storitest.com>", "SMTP sender")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	formater := mailer.NewFormater()
+	mailer := mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender)
 
 	app := &application{
 		config:    cfg,
 		logger:    logger,
 		csvLoader: *reader.NewCsvDataReader(),
 		parser:    parser.NewTransactionParser(),
+		mailer:    mailer,
+		formater:  formater,
 	}
 
 	srv := &http.Server{
