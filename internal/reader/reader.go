70b453a8689f3da6a675895e60da5eaf436c1f61
Alejandro Henestroza <alejandro.henestroza@mercadolibre.com>
2023-11-13 16:06:19 -0300

    feat: make DataReader interface

--- internal/reader/reader.go
+++ internal/reader/reader.go
@@ -1,30 +1,7 @@
 package reader
 
-import (
-	"errors"
-	"os"
-	"strings"
-)
+import "os"
 
-type CsvDataReader struct {
-	Records []string
-}
-
-func NewCsvDataReader() *CsvDataReader {
-	return &CsvDataReader{}
-}
-
-func (csvl *CsvDataReader) Read(path string) ([]string, error) {
-	file, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	rawString := string(file)
-	csvl.Records = strings.Split(rawString, "\n")[1:] // Skip first line, which contains the headers of the CSV file
-
-	if len(csvl.Records) == 0 {
-		return nil, errors.New("no records present")
-	}
-	return csvl.Records, nil
+type DataReader interface {
+	ReadFile(file *os.File) ([]string, error)
 }
