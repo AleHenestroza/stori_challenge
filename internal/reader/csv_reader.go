c18f1fb983b905dadecc204b746981f461253c1b
Alejandro Henestroza <alejandro.henestroza@mercadolibre.com>
2023-11-13 16:06:41 -0300

    feat: add csv_reader. Read from files instead of raw strings

---
+++ internal/reader/csv_reader.go
@@ -0,0 +1,29 @@
+package reader
+
+import (
+	"os"
+	"strings"
+)
+
+type CsvDataReader struct{}
+
+func NewCsvDataReader() *CsvDataReader {
+	return &CsvDataReader{}
+}
+
+func (r *CsvDataReader) ReadFile(file *os.File) ([]string, error) {
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	buf := make([]byte, fileInfo.Size())
+	_, err = file.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	text := string(buf)
+	rows := strings.Split(text, "\n")
+	return rows, nil
+}
