a834cd8f209c845f3f5b5edb55f33f3c112bf34d
Alejandro Henestroza <alehenestroza@gmail.com>
2023-11-14 12:56:16 -0300

    [FTR/FIX] Add waitgroup, graceful shutdown, fix saveTransactions (#29)
    * fix: fix upload csv file
    
    * feat: add waitgroup, send emails async
    
    * fix: fix collection endpoint method

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,9 +6,9 @@ import (
 	"flag"
 	"fmt"
 	"log/slog"
-	"net/http"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/alehenestroza/stori-backend-challenge/internal/data"
@@ -45,6 +45,7 @@ type application struct {
 	parser parser.TransactionParser
 	mailer mailer.Mailer
 	models data.Models
+	wg     sync.WaitGroup
 }
 
 func main() {
@@ -85,19 +86,11 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	err = app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
-
-	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-	err = srv.ListenAndServe()
-	logger.Error(err.Error())
-	os.Exit(1)
 }
 
 func buildSmtpStruct() (smtp, error) {
