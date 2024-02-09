76cacd2ee96cc0e6d14107dd79bfcfa85af0db14
Alejandro Henestroza <alehenestroza@gmail.com>
2024-02-09 17:03:10 -0300

    qweqwe

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -14,6 +14,7 @@ import (
 const (
 	ScopeActivation     = "activation"
 	ScopeAuthentication = "authentication"
+	ScopeUnblock        = "unblock"
 	// TODO: Implement -> ScopePasswordReset  = "password-reset"
 )
 
